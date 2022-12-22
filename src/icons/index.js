2e257d8c0e5ff5833e73e727f4178d77978f5081
laijinchang <2365039106@qq.com>
2022-12-22 17:10:40 +0800

    第一次提交

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+//以上两行代码的含义就是将所有。svg的后缀名全部映入项目
